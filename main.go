34a0366fccdfa326cc509fec807b80882f7ae722
Jacques du Plessis <jaakidup@gmail.com>
2021-08-29 14:34:28 +0200

    Typo in comment.

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 
 	const name = "Onion"
 	const port = 8000
-	const host = "127.0.0.1"
+	const host = "localhost"
 
 	fmt.Printf("Starting %v on host:port %v:%v\n", name, host, port)
 	server := Server{Name: name, Host: host, Port: port}
